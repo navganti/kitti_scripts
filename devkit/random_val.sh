be5fd6f50728521807f713eb8eaaaa31a4a5d599
Jonathan Smith <jajsmith@edu.uwaterloo.ca>
2018-05-18 00:03:43 -0400

    Updated validation/training split script

--- devkit/random_val.sh
+++ devkit/random_val.sh
@@ -7,20 +7,36 @@
 #    val/
 #    valannot
 #
-# With train containing all the training images
-# and trainannot containing all the labeled training images
+# With val containing all the training images
+# and valannot containing all the labeled training images
 #
 # Then the script will take a random subset (20% of 200 images = 40)
-# and move them into the validation folder
+# and move them into the train folders
 
-cd train
-FILES=$(ls . | shuf -n 40)
-for f in $FILES; do
-    mv "$f" ../val/
+FILES=$(cat val.txt | shuf)
+COUNT=0
+
+for line in $FILES; do
+    echo "line: $line"
+
+    if (( COUNT < 80 )); then
+	if [[ $line = *"TrainIds"* ]]; then
+	    mv "$line" ./trainannot/
+	else
+	    mv "$line" ./train/
+	fi
+    	echo $line >> train.txt
+    else
+    	echo $line >> val-new.txt
+    fi
+    COUNT=$((COUNT+1))
 done
 
-cd ../trainannot
-for f in $FILES; do
-    mv "$f" ../valannot/
-done
+# Put them on the same line
+sed 'N;s/\n/ /' train.txt | tee train-new-oneline.txt
+sed 'N;s/\n/ /' val-new.txt | tee val-new-oneline.txt
 
+rm train.txt
+mv train-new-oneline.txt train.txt
+rm val.txt val-new.txt
+mv val-new-oneline.txt val.txt
